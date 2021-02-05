3fb4fa362e22aaa7c4f6b9b2f8bbea20643a8b0a
anderzzz <ohrn.anders@gmail.com>
2021-02-05 17:42:42 +0100

    Refactor with better class abstraction, plus modify the denoising and thresholding of image to derive mask

--- main.py
+++ main.py
@@ -4,14 +4,27 @@
 from skimage.io import imread
 
 from train_data import parse_labels, factory
-from segmentor import ConfocalCellSegmentor, ConfocalNucleusSegmentor
+from segmentor import ConfocalNucleusAreaMasker, ConfocalNucleusSegmentor, \
+                      ConfocalCellAreaMasker, ConfocalCellSegmentor
 from shaper import ImageShapeMaker
 from visualiser import Visualiser
 
 local_imgs = factory.create('local disk', src_dir='./data_tmp')
+
+masker_nuc = ConfocalNucleusAreaMasker(img_data_accessor=local_imgs, reader_func=imread,
+                                       edge_width=10,
+                                       body_luminosity=25, body_object_area=5000, body_hole_area=5000,
+                                       edge_luminosity=25, edge_object_area=6, edge_hole_area=5000)
 segmentor_nuc = ConfocalNucleusSegmentor(img_data_accessor=local_imgs, reader_func=imread)
-segmentor_cell = ConfocalCellSegmentor(img_data_accessor=local_imgs, reader_func=imread,
-                                       nucleus_segmentor=segmentor_nuc)
+
+masker_cell = ConfocalCellAreaMasker(img_data_accessor=local_imgs, reader_func=imread,
+                                     body_luminosity=0, body_object_area=100, body_hole_area=100)
+segmentor_cell = ConfocalCellSegmentor(img_data_accessor=local_imgs, reader_func=imread)
+
+#segmentor_nuc = ConfocalNucleusSegmentor(img_data_accessor=local_imgs, reader_func=imread, edge_width=10)
+#segmentor_cell = ConfocalCellSegmentor(img_data_accessor=local_imgs, reader_func=imread,
+#                                       nucleus_segmentor=segmentor_nuc,
+#                                       body_luminosity=30, body_hole_area=20000, body_object_area=10000)
 shaper_cell = ImageShapeMaker(img_data_accessor=local_imgs, reader_func=imread)
 viz = Visualiser(cmap='gray')
 
@@ -19,21 +32,31 @@ df_labels = parse_labels('./data_tmp/train.csv')
 
 for cell_id, data_path_collection in local_imgs.items():
 
+    if not '000a' in cell_id:
+        continue
+
     img_nuc = data_path_collection['nuclei']
     img_er = data_path_collection['ER']
+    img_tube = data_path_collection['red']
     img_prot = data_path_collection['green']
 
-    segmentor_cell.segment(img_er, img_nuc).prune()
+    masker_nuc.make_mask_(img_nuc)
+    segmentor_nuc.make_segments_(img_nuc, masker_nuc.mask)
 
-    shaper_cell.apply_to(img_prot, segmentor_cell.mask).cut_square()
+    masker_cell.make_mask_(img_tube, masker_nuc.mask)
+    segmentor_cell.make_segments_(img_er, masker_cell.mask, masker_nuc.mask, segmentor_nuc.segments)
+
+    raise RuntimeError
+
+    shaper_cell.apply_to(img_prot, segmentor_cell.mask).outline()
     percell_prot = shaper_cell.cell_images_reshaped.copy()
-    shaper_cell.apply_to(img_er, segmentor_cell.mask).cut_square()
+    shaper_cell.apply_to(img_er, segmentor_cell.mask).outline()
     percell_er = shaper_cell.cell_images_reshaped.copy()
 
-    #for cell_counter in percell_prot.keys():
-    #    viz.plot_(percell_prot[cell_counter], percell_er[cell_counter])
-    #viz.show_(*tuple(percell_er.values()))
-    
+    for cell_counter in percell_prot.keys():
+        viz.show_(percell_prot[cell_counter], percell_er[cell_counter])
+    viz.show_(*tuple(percell_er.values()))
+
     print (df_labels.loc[cell_id])
 
 
--- segmentor.py
+++ segmentor.py
@@ -3,10 +3,11 @@
 '''
 import numpy as np
 
-from skimage.segmentation import watershed
+from skimage.segmentation import watershed, random_walker
 from skimage.draw import ellipse
 from skimage import morphology
 from skimage.measure import EllipseModel
+from skimage.util import invert
 from scipy import ndimage
 
 import matplotlib.pyplot as plt
@@ -20,7 +21,11 @@ def ellipse_fits(img_labelled):
         rows, cols = np.where(img_labelled == k_label)
         coords = np.stack([rows, cols]).T
         ellipse = EllipseModel()
-        ellipse.estimate(coords)
+        estimate_success = ellipse.estimate(coords)
+
+        if not estimate_success:
+            xc, yc = tuple([int(x) for x in np.median(coords, axis=0)])
+            ellipse.params = xc, yc, 10, 10, 0.0
 
         ellipses_by_label[k_label] = ellipse
 
@@ -64,37 +69,129 @@ def nuclei_edge_condition(nuclei_id_pixels, thickness=1):
 
     return on_edge
 
-class _ConfocalSegmentor(object):
+class _ConfocalWorker(object):
     '''Bla bla
 
     '''
-    def __init__(self, img_data_accessor, reader_func, reader_func_kwargs={}):
+    def __init__(self, img_data_accessor, reader_func, reader_func_kwargs):
         self.img_data_accessor = img_data_accessor
         self.reader_func = reader_func
         self.reader_func_kwargs = reader_func_kwargs
 
-    def _denoise_and_thrs(self, img, thrs_value, max_area_object, max_area_hole):
-        return morphology.remove_small_holes(
-                   morphology.remove_small_objects(img > thrs_value, max_area_object), max_area_hole)
+    def retrieve_image(self, img_path):
+        '''Bla bla
 
-class ConfocalNucleusSegmentor(_ConfocalSegmentor):
+        '''
+        return self.reader_func(img_path, **self.reader_func_kwargs)
+
+class _ConfocalAreaMasker(_ConfocalWorker):
+    '''Bla bla
+
+    '''
+    def __init__(self, img_data_accessor, reader_func, reader_func_kwargs,
+                       edge_width=None,
+                       body_luminosity=None, body_object_area=None, body_hole_area=None,
+                       edge_luminosity=None, edge_object_area=None, edge_hole_area=None):
+
+        super().__init__(img_data_accessor, reader_func, reader_func_kwargs)
+        self.edge_width = edge_width
+        self.body_luminosity = body_luminosity
+        self.body_object_area = body_object_area
+        self.body_hole_area = body_hole_area
+        self.edge_luminosity = edge_luminosity
+        self.edge_object_area = edge_object_area
+        self.edge_hole_area = edge_hole_area
+
+        self.mask = None
+
+    def denoise_and_thrs(self, img):
+        '''Bla bla
+
+        '''
+        if self.edge_width is None:
+            mask = self._denoise_thrs(img, self.body_luminosity, self.body_object_area, self.body_hole_area)
+
+        else:
+            mask_body = self._denoise_thrs(img, self.body_luminosity, self.body_object_area, self.body_hole_area)
+            mask_edge = self._denoise_thrs(img, self.edge_luminosity, self.edge_object_area, self.edge_hole_area)
+
+            body_mask = np.ones(img.shape, dtype=np.bool)
+            body_mask[:self.edge_width + 1, :] = False
+            body_mask[-self.edge_width - 1:, :] = False
+            body_mask[:, :self.edge_width + 1] = False
+            body_mask[:, -self.edge_width - 1:] = False
+            mask = np.where(body_mask, mask_body, mask_edge)
+
+        return mask
+
+    def _denoise_thrs(self, img, thrs, max_object, max_hole):
+        return morphology.remove_small_holes(
+                   morphology.remove_small_objects(img > thrs, max_object), max_hole)
+
+
+class _ConfocalMaskSegmentor(_ConfocalWorker):
+    '''Bla bla
+
+    '''
+    def __init__(self, img_data_accessor, reader_func, reader_func_kwargs):
+
+        super().__init__(img_data_accessor, reader_func, reader_func_kwargs)
+
+        self.mask_segment_ = {}
+        self.segments = None
+
+    def cmp_mask_segments(self):
+
+        for cell_counter in range(1, np.max(self.segments) + 1):
+            self.mask_segment_[cell_counter] = self.segments == cell_counter
+
+    @property
+    def n_segments(self):
+        len(self.mask_segment_)
+
+    def __getitem__(self, item):
+        return self.mask_segment_[item]
+
+    def items(self):
+        for key, value in self.mask_segment_.items():
+            yield key, value
+
+
+class ConfocalNucleusAreaMasker(_ConfocalAreaMasker):
+    '''Bla bla
+    
+    '''
+    def __init__(self, img_data_accessor, reader_func, reader_func_kwargs={},
+                 edge_width=None,
+                 body_luminosity=25, body_object_area=5000, body_hole_area=5000,
+                 edge_luminosity=25, edge_object_area=6, edge_hole_area=5000):
+
+        super().__init__(img_data_accessor, reader_func, reader_func_kwargs,
+                         edge_width,
+                         body_luminosity, body_object_area, body_hole_area,
+                         edge_luminosity, edge_object_area, edge_hole_area)
+        
+    def make_mask_(self, img_path):
+        '''Bla bla
+        
+        '''
+        img = self.retrieve_image(img_path)
+        self.mask = self.denoise_and_thrs(img)
+        
+
+class ConfocalNucleusSegmentor(_ConfocalMaskSegmentor):
     '''Bla bla
 
     '''
     def __init__(self, img_data_accessor, reader_func, reader_func_kwargs={},
-                 denoise_upper_luminosity=25, denoise_object_area=5000, denoise_hole_area=5000):
+                 fit_ellipses=True):
 
         super().__init__(img_data_accessor, reader_func, reader_func_kwargs)
-        self.denoise_upper_luminosity = denoise_upper_luminosity
-        self.denoise_object_area = denoise_object_area
-        self.denoise_hole_area = denoise_hole_area
+        self.fit_ellipses = fit_ellipses
 
-        self.mask = None
-        self.segments_id = None
-        self.n_segments = None
-        self.segments_ellipse = None
+        self.segments_ellipse = {}
 
-    def segment(self, img_nuclei_path):
+    def make_segments_(self, img_nuclei_path, nuclei_mask):
         '''Segment the plurality of nuclei in image, and fit ellipse
 
         This is done by (1) moderately denoise image by removing holes and objects, (2) identify each nucleus
@@ -103,10 +200,15 @@ class ConfocalNucleusSegmentor(_ConfocalSegmentor):
         '''
         img_nuclei = self.retrieve_image(img_nuclei_path)
 
-        self.mask = self._denoise_and_thrs(img_nuclei, self.denoise_upper_luminosity,
-                                           self.denoise_object_area, self.denoise_hole_area)
-        self.segments_id, self.n_segments = ndimage.label(self.mask)
-        self.segments_ellipse = ellipse_fits(self.segments_id)
+        self.segments, _ = ndimage.label(nuclei_mask)
+        self.cmp_mask_segments()
+        if self.fit_ellipses:
+            self.segments_ellipse = ellipse_fits(self.segments)
+
+        fig, ax = plt.subplots(1,1)
+        ax.imshow(img_nuclei, cmap='gray')
+        ax.imshow(self.segments, cmap=plt.cm.jet)
+        plt.show()
 
     def bounding_ellipse(self, bounding_n_neighbours=4, bounding_radial_slack=0.0):
         '''Construct bounding ellipse around each nucleus
@@ -114,30 +216,38 @@ class ConfocalNucleusSegmentor(_ConfocalSegmentor):
         '''
         return bounding_ellipses(self.segments_ellipse, bounding_n_neighbours, bounding_radial_slack)
 
-    def retrieve_image(self, img_nuclei_path):
+class ConfocalCellAreaMasker(_ConfocalAreaMasker):
+    '''Bla bla
+    
+    '''
+    def __init__(self, img_data_accessor, reader_func, reader_func_kwargs={},
+                       body_luminosity=0, body_object_area=100, body_hole_area=100):
+        
+        super().__init__(img_data_accessor, reader_func, reader_func_kwargs,
+                         body_luminosity=body_luminosity, body_object_area=body_object_area, body_hole_area=body_hole_area)
+        
+    def make_mask_(self, img_path, nuclei_mask):
         '''Bla bla
-
+        
         '''
-        return self.reader_func(img_nuclei_path, **self.reader_func_kwargs)
+        img = self.reader_func(img_path, **self.reader_func_kwargs)
+        img[nuclei_mask] = 255
+        
+        img_thrs = self.denoise_and_thrs(img)
+        self.mask = morphology.binary_dilation(img_thrs, selem=morphology.disk(12))
 
-class ConfocalCellSegmentor(_ConfocalSegmentor):
+
+class ConfocalCellSegmentor(_ConfocalMaskSegmentor):
     '''Bla bla
 
     '''
     def __init__(self, img_data_accessor, reader_func, reader_func_kwargs={},
-                 nucleus_segmentor=None,
-                 prune_min_area=500, prune_entire_nucleus=True,
-                 denoise_lower_luminosity=50, denoise_object_area=16, denoise_hole_area=16):
+                 prune_min_area=500, prune_entire_nucleus=True):
 
         super().__init__(img_data_accessor, reader_func, reader_func_kwargs)
-        self.nucleus_segmentor = nucleus_segmentor
+
         self.prune_min_area = prune_min_area
         self.prune_entire_nucleus = prune_entire_nucleus
-        self.denoise_lower_luminosity = denoise_lower_luminosity
-        self.denoise_object_area = denoise_object_area
-        self.denoise_hole_area = denoise_hole_area
-
-        self.mask = {}
 
     def prune(self):
         '''Bla bla
@@ -158,43 +268,59 @@ class ConfocalCellSegmentor(_ConfocalSegmentor):
 
         return self
 
-    def segment(self, img_er_path, img_nuclei_path):
+    def make_segments_(self, img_path, cell_mask, nuclei_mask, nuclei_segments):
 
-        # Segment nucleus
-        self.nucleus_segmentor.segment(img_nuclei_path)
-        nucleus_bounding_ellipses = self.nucleus_segmentor.bounding_ellipse()
+        img = self.retrieve_image(img_path)
+        img[nuclei_mask] = 255
+        print (nuclei_segments)
 
-        # Add nuclei at maximum intensity to ER image
-        img_cell = self.reader_func(img_er_path, **self.reader_func_kwargs)
-        img_cell[self.nucleus_segmentor.mask] = 255
-        height, width = img_cell.shape
+        self.segments = watershed(invert(img), markers=nuclei_segments, mask=cell_mask, compactness=0)
+        self.cmp_mask_segments()
+        print (np.max(self.segments))
+        print (self.mask_segment_.keys())
 
-        # Threshold the image
-        img_thrs = self._denoise_and_thrs(img_cell, self.denoise_lower_luminosity,
-                                          self.denoise_object_area, self.denoise_hole_area)
+        print (nuclei_segments)
 
-        # Run segmentation on masked image data
-        for cell_counter, ellipse_b in nucleus_bounding_ellipses.items():
+        for cell_counter, the_mask in self.items():
+            print ('AA:{}'.format(cell_counter))
+            fig, ax = plt.subplots(1,2)
+            ax[0].imshow(img, cmap='gray')
+            ax[0].imshow(the_mask, alpha=0.5, cmap=plt.cm.jet)
+            ax[1].imshow(cell_mask)
+            plt.show()
 
-            # Mask for content within bounding ellipse (and within frame of image)
-            ellipse_kwargs = dict(zip(('r', 'c', 'r_radius', 'c_radius', 'rotation'), ellipse_b.params))
-            ellipse_kwargs['rotation'] = ellipse_kwargs['rotation'] * -1.0
-            rr, cc = ellipse(**ellipse_kwargs)
-
-            mask_inside_frame = ((rr >= 0) & (rr < height)) & ((cc >= 0) & (cc < width))
-            rr = rr[mask_inside_frame]
-            cc = cc[mask_inside_frame]
-
-            mask_cell_area = np.zeros(img_cell.shape, dtype=np.bool)
-            mask_cell_area[rr, cc] = True
-
-            # Initialize cell labels, one unique value per nucleus within bounding ellipse mask
-            labels = np.copy(self.nucleus_segmentor.segments_id)
-            labels[~mask_cell_area] = -1
-
-            segmented = watershed(~img_thrs, labels)
-            self.mask[cell_counter] = segmented == labels[ellipse_kwargs['r'], ellipse_kwargs['c']]
-
+        #for cell_counter, ellipse_b in nucleus_bounding_ellipses.items():
+#
+#            # Mask for content within bounding ellipse (and within frame of image)
+#            ellipse_kwargs = dict(zip(('r', 'c', 'r_radius', 'c_radius', 'rotation'), ellipse_b.params))
+#            ellipse_kwargs['rotation'] = ellipse_kwargs['rotation'] * -1.0
+#            rr, cc = ellipse(**ellipse_kwargs)
+#
+#            mask_inside_frame = ((rr >= 0) & (rr < height)) & ((cc >= 0) & (cc < width))
+#            rr = rr[mask_inside_frame]
+#            cc = cc[mask_inside_frame]
+#
+#            mask_cell_area = np.zeros(img_er.shape, dtype=np.bool)
+#            mask_cell_area[rr, cc] = True
+#            img_mask_slice = np.where(mask_cell_area, img_thrs, False)
+##            mask_cell_area = np.zeros(img_cell.shape, dtype=np.bool)
+##            mask_cell_area[self.nucleus_segmentor.segments_id == cell_counter + 1] = True
+#
+#            # Initialize cell labels, one unique value per nucleus within bounding ellipse mask
+#            labels = np.copy(self.nucleus_segmentor.segments_id)
+#            #labels[~mask_cell_area] = -1
+#
+#            #segmented = random_walker(~img_thrs, labels, beta=130)
+#            segmented = watershed(invert(img_er), labels, compactness=0, mask=img_mask_slice)
+#            #self.mask[cell_counter] = segmented == labels[ellipse_kwargs['r'], ellipse_kwargs['c']]
+#            self.mask[cell_counter] = segmented == cell_counter
+#
+#            fig, ax = plt.subplots(1,3)
+#            ax[0].imshow(img_er, cmap='gray')
+#            ax[0].imshow(self.mask[cell_counter], alpha=0.5, cmap=plt.cm.jet)
+#            ax[1].imshow(img_thrs)
+#            ax[2].imshow(img_mask_slice)
+#            plt.show()
 
         #fig, ax = plt.subplots(2,2)
         #ax[0,0].imshow(img_cell, cmap='gray')
