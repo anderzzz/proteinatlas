73b8f4d91a059d375b6650030d353b7a5585f4fd
anderzzz <ohrn.anders@gmail.com>
2021-02-06 15:16:00 +0100

    Handle the colormap properly plus deal with case of one row of images

--- visualiser.py
+++ visualiser.py
@@ -1,14 +1,19 @@
 '''Bla bla
 
 '''
+import copy
 import numpy as np
 import matplotlib.pyplot as plt
 
 class Visualiser(object):
 
-    def __init__(self, intensity_range=(0, 255), cmap='gray'):
+    def __init__(self, intensity_range=(0, 255), cmap='jet', cmap_set_under='black', cmap_set_over='white'):
         self.intensity_range = intensity_range
-        self.cmap = cmap
+        self.cmap = copy.copy(plt.cm.get_cmap(cmap))
+        if not cmap_set_under is None:
+            self.cmap.set_under(cmap_set_under)
+        if not cmap_set_over is None:
+            self.cmap.set_over(cmap_set_over)
 
     def show_(self, *args):
 
@@ -17,9 +22,12 @@ class Visualiser(object):
         ncol = int(np.ceil(n_imgs / nrow))
         fig, ax = plt.subplots(nrow, ncol)
         for k_img, img in enumerate(args):
-            k_row = k_img // ncol
-            k_col = k_img % ncol
-            ax[k_row, k_col].imshow(img, cmap=self.cmap, vmin=self.intensity_range[0], vmax=self.intensity_range[1])
+            if nrow > 1:
+                k_row = k_img // ncol
+                k_col = k_img % ncol
+                ax[k_row, k_col].imshow(img, cmap=self.cmap, vmin=self.intensity_range[0], vmax=self.intensity_range[1])
+            else:
+                ax[k_img].imshow(img, cmap=self.cmap, vmin=self.intensity_range[0], vmax=self.intensity_range[1])
 
         plt.show()
 
